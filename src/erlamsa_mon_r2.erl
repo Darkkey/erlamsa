643b6ff294b39d66835f094eee92c152835282e7
Darkkey <alexanderdarkkey@gmail.com>
2018-06-10 20:09:46 +0200

    r2 monitor seems to be more or less workable (OS X)

--- src/erlamsa_mon_r2.erl
+++ src/erlamsa_mon_r2.erl
@@ -1,17 +1,17 @@
 -module(erlamsa_mon_r2).           
--export([start/1, start/2, stop/1, init/2, call_port/2]).
+
+-include("erlamsa.hrl").
+
+-export([start/1, init/1]).
 
 parse_params(["after_params=" ++ AfterParam|T], Acc) ->
     parse_params(T, maps:put(do_after_params, AfterParam, Acc));
 parse_params(["after=" ++ AfterType|T], Acc) ->
     parse_params(T, maps:put(do_after_type, list_to_atom(AfterType), Acc));
-%%TODO:add launch in addition to attach
 parse_params(["app=" ++ App|T], Acc) ->
     parse_params(T, maps:put(app, App, Acc));
 parse_params(["r2path=" ++ R2Path|T], Acc) ->
     parse_params(T, maps:put(r2path, R2Path, Acc));
-parse_params(["platform=" ++ Platform|T], Acc) ->
-    parse_params(T, maps:put(platform, list_to_atom(Platform), Acc));
 parse_params([_H|T], Acc) ->
     %%TODO: do it in more elegant way
     io:format("Invalid monitor parameter: ~p, skipping...", [T]),
@@ -24,95 +24,62 @@ do_after(exec, Opts) ->
     os:cmd(ExecPath); %%TODO: add result to logs
 do_after(nil, _Opts) -> ok.
 
-start(Params) -> 
-    MonOpts = parse_params(string:split(Params,",",all), maps:new()),
-    %%TODO:handle wrong params
-    R2Cmd = io_lib:format("~s -q0 -d ~s", [maps:get(r2path, MonOpts, "r2"), maps:get(app, MonOpts, "")]),
+handle_r2_session(start, nil, R2Cmd) ->
     erlamsa_logger:log(info, "r2monitor attempting to run: '~s'", [R2Cmd]),
-    R2Pid = start(R2Cmd, []), %%TODO: windows platform handling
-    io:format("!!!!!!!\n"),
-    Res = call_port(R2Pid, "ij\r\n"), %%autoanalysis
-    io:format("/!!!!!!!\n"),
-    io:format("Tmpres: ~s", [Res]),
-    Res = call_port(R2Pid, "aaa\n"), %%autoanalysis
-    io:format("Tmpres: ~s", [Res]),
-    Res2 = call_port(R2Pid, "dc\n"), %%TODO: handle not started state
-    io:format("Tmpres: ~s", [Res2]),
-    CrashMsg = call_port(R2Pid, "dc\n"), %% in loop run dc until everything is ok
-    %% here there is crash
+    {R2Pid, StartRes} = r2pipe:start(R2Cmd, []), %%TODO: windows platform handling? Do we need it or need to fix r2?
+    handle_r2_session(started, R2Pid, StartRes);
+handle_r2_session(started, exit, StartRes) ->
+    erlamsa_logger:log(error, "R2 unexpectedly exit while starting application: ~s~n", [lists:flatten(StartRes)]),
+    error;
+handle_r2_session(started, R2Pid, StartRes = ["= attach" ++ _Pids| _T]) ->
+    erlamsa_logger:log(info, "r2monitor r2 start messages: '~s'", [lists:flatten(StartRes)]),
+    _AAARes = r2pipe:call(R2Pid, "aaa\n"), %%autoanalysis
+    RunRes = r2pipe:call(R2Pid, "dc\n"), 
+    handle_r2_session(continue, R2Pid, RunRes);
+handle_r2_session(started, R2Pid, StartRes) ->
+    erlamsa_logger:log(error, "Error while starting application: ~p ~n", [lists:flatten(StartRes)]),
+    r2pipe:stop(R2Pid),
+    error;
+handle_r2_session(continue, R2Pid, [10] ++ "==> Process finished" ++ [10,10]) ->
+    r2pipe:stop(R2Pid),
+    erlamsa_logger:log(info, "r2monitor ==> Process finished without error code, is it ok or crash?", []),
+    ok;
+handle_r2_session(continue, R2Pid, CrashMsg) ->
     erlamsa_logger:log(info, "r2monitor [-->!!!<--] detected event (CRASH?!): ~sp", [CrashMsg]),
-    RegInfoMsg = call_port(R2Pid, "drj\n"),
+    RegInfoMsg = r2pipe:call(R2Pid, "drj\n"),
     erlamsa_logger:log(info, "r2monitor registers info ~s", [RegInfoMsg]),
-    Info = call_port(R2Pid, "ij\n"),
+    Info = r2pipe:call(R2Pid, "ij\n"),
     erlamsa_logger:log(info, "r2monitor executable info ~s", [Info]),
-    MemMaps = call_port(R2Pid, "dmj\n"),
-    erlamsa_logger:log_data(info, "r2monitor memory maps ~s", [], MemMaps),
-    %BackTraceJson = erlamsa_simple_port:call_port("dbtj\n"),
+    %%MemMaps = r2pipe:call(R2Pid, "dmj\n"),
+    %% FIXME: should we record memory maps now or later? because they seems to be empty after crash...
+    %erlamsa_logger:log_data(info, "r2monitor memory maps recorded", [], list_to_binary(MemMaps)),
+    %BackTraceJson = r2pipe:call("dbtj\n"),
     %erlamsa_logger:log(info, "r2monitor json backtrace ~s", [BackTraceJson]),
-    BackTrace = call_port(R2Pid, "dbt\n"),
+    BackTrace = r2pipe:call(R2Pid, "dbt\n"),
     erlamsa_logger:log(info, "r2monitor backtrace ~s", [BackTrace]),
-    stop(R2Pid),
-    do_after(maps:get(do_after_type, MonOpts, nil), MonOpts),
-    start(MonOpts). %% TODO:ugly, rewrite
-
-start(ExtPrg, ExtraParams) ->
-    spawn(?MODULE, init, [ExtPrg, ExtraParams]).
-stop(Pid) ->
-    Pid ! stop.
-
-call_port(Pid, Msg) ->
-    Pid ! {call, self(), Msg},
-    receive
-        {Pid, Result} ->
-            Result
-    end.
-
-receive_port(Port) ->
-    receive
-		{Port, {data, Data}} ->
-            Data
-	end.
-
-%%TODO:FIXME:Very ugly, thx to r2 weirdiness, this may be slow and unefficient.
-read_r2_data(Port) ->
-    read_r2_data(Port, none, []).
-read_r2_data(Port, 0, [[0]] ) ->
-    Data = receive_port(Port),
-    read_r2_data(Port, lists:nth(length(Data), Data), [Data]);
-read_r2_data(_Port, 0, Acc) ->
-    lists:droplast(lists:flatten(lists:reverse(Acc)));
-read_r2_data(Port, _Any, Acc) ->
-    Data = receive_port(Port),
-    read_r2_data(Port, lists:nth(length(Data), Data), [Data | Acc]).
-
-r2_ready(Port, start) ->
-    [H|T] = receive_port(Port),
-    r2_ready(Port, H);
-r2_ready(Port, 0) ->
+    r2pipe:stop(R2Pid),
+    erlamsa_logger:log(info, "r2monitor debugging finished", []),
     ok.
 
-loop(Port) ->
-    receive
-	{call, Caller, Msg} ->
-	    Port ! {self(), {command, Msg}},
-      	    Caller ! {erlamsa_mon_r2, read_r2_data(Port)},
-	    loop(Port);
-	stop ->
-	    Port ! {self(), close},
-	    receive
-            {Port, closed} ->
-                exit(normal)
-	    end;
-	{'EXIT', Port, _Reason} ->
-	    exit(port_terminated)
-    end.   
 
-init(ExtPrg, ExtraParams) ->
-    process_flag(trap_exit, true),
-    Port = open_port({spawn, ExtPrg}, [use_stdio, stderr_to_stdout, hide, stream | ExtraParams]),
-    ok = r2_ready(Port, start),
-    io:format("r2 ready!"),
-    loop(Port). 
+start(Params) -> 
+   Pid = spawn(?MODULE, init, [Params]),
+   {ok, Pid}.
 
+init(Params) ->
+    MonOpts = parse_params(string:split(Params,",",all), maps:new()),
+    r2_launch(MonOpts, 0).
 
+r2_launch(_MonOpts, N = ?START_MONITOR_ATTEMPTS) ->
+    erlamsa_logger:log(info, "r2_monitor: too many failures (~p), giving up", [N]);
+r2_launch(MonOpts, N) ->
+    %%TODO:handle wrong params
+    R2Cmd = io_lib:format("~s -q0 -d ~s", [maps:get(r2path, MonOpts, "r2"), maps:get(app, MonOpts, "")]),
+    case handle_r2_session(start, nil, R2Cmd) of %% TODO: do after only if res is ok
+        ok ->
+            do_after(maps:get(do_after_type, MonOpts, nil), MonOpts),
+            r2_launch(MonOpts, 0);
+        error ->
+            r2_launch(MonOpts, N+1)
+    end. %% TODO:ugly, rewrite
 
---
+++ src/r2pipe.erl
@@ -0,0 +1,114 @@
+% Copyright (c) 2015-2018 Alexander Bolshev aka dark_k3y
+%
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+%
+% The above copyright notice and this permission notice shall be included in
+% all copies or substantial portions of the Software.
+%
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+% SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+% DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+% OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+% THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+%
+%%%-------------------------------------------------------------------
+%%% @author dark_k3y
+%%% @doc
+%%% Updated R2Pipe binding for radare2
+%%% @end
+%%%-------------------------------------------------------------------
+-module(r2pipe).
+-export([start/2, stop/1, call/2, init/3]).
+
+start(ExtPrg, ExtraParams) ->
+    Pid = spawn(?MODULE, init, [self(), ExtPrg, ExtraParams]),
+    receive
+        {Pid, Result} ->
+            {Pid, Result};
+        {Pid, exit, Result} ->
+            {exit, Result}
+    end.
+
+stop(Pid) ->
+    Pid ! stop.
+
+call(Pid, Msg) ->
+    Pid ! {call, self(), Msg},
+    receive
+        {Pid, Result} ->
+            Result
+    end.
+
+receive_port(Port) ->
+    receive
+		{Port, {data, Data}} ->
+            Data;
+        {'EXIT', Port, _Reason} ->
+            port_exit 
+	end.
+
+%%TODO:FIXME:Very ugly, thx to r2 weirdiness, this may be slow and unefficient.
+read_r2_data(Port) ->
+    read_r2_data(Port, none, []).
+read_r2_data(Port, 0, [[0]] ) ->
+    Data = receive_port(Port),
+    %io:format("1) ~p~n", [Data]),
+    read_r2_data(Port, lists:nth(length(Data), Data), [Data]);
+read_r2_data(_Port, 0, Acc) ->
+    %io:format("2) ~p~n", [nil]),
+    lists:droplast(lists:flatten(lists:reverse(Acc)));
+read_r2_data(Port, _Any, Acc) ->
+    Data = receive_port(Port),
+    %io:format("3) ~p ~p~n", [Data, lists:nth(length(Data), Data)]),
+    read_r2_data(Port, lists:nth(length(Data), Data), [Data | Acc]).
+
+
+% waiting while r2 returns \0 byte -- signal that it has been started
+r2_ready(_Port, [0], Acc) ->
+    {ok, lists:reverse(Acc)};
+r2_ready(Port, _PrevData, Acc) ->
+    case receive_port(Port) of
+        port_exit ->
+            {exit, lists:reverse(Acc)};
+        Data -> 
+            %io:format("Start: ~p  ~p~n", [Data, lists:nth(length(Data), Data)]),
+            r2_ready(Port, Data, [Data | Acc])
+    end.
+
+loop(Port) ->
+    receive
+	{call, Caller, Msg} ->
+	    Port ! {self(), {command, Msg}},
+        RcvdData = read_r2_data(Port),
+        %io:format("******* RcvData: ~p~n", [RcvdData]),
+   	    Caller ! {self(), RcvdData},
+	    loop(Port);
+	stop ->
+	    Port ! {self(), close},
+	    receive
+            {Port, closed} ->
+                exit(normal)
+	    end;
+	{'EXIT', Port, _Reason} ->
+	    exit(port_terminated)
+    end.   
+
+init(StarterPid, ExtPrg, ExtraParams) ->
+    process_flag(trap_exit, true),
+    Port = open_port({spawn, ExtPrg}, [use_stdio, stderr_to_stdout, hide, stream | ExtraParams]),
+    case r2_ready(Port, [], []) of
+        {ok, Data} ->
+            StarterPid ! {self(), Data},
+            loop(Port);
+        {exit, ExitData} ->
+            StarterPid ! {self(), exit, ExitData}
+    end.
+
+
