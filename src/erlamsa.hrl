0eb654d7f0bda3a0ed5bc6b8847f515549522372
Darkkey <alexanderdarkkey@gmail.com>
2019-01-23 18:59:20 +0100

    Added erlserial dependency.

--- src/erlamsa.hrl
+++ src/erlamsa.hrl
@@ -1,15 +1,36 @@
+% Copyright (c) 2011-2014 Aki Helin
+% Copyright (c) 2014-2019 Alexander Bolshev aka dark_k3y
+%
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+%
+% The above copyright notice and this permission notice shall be included in
+% all copies or substantial portions of the Software.
+%
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+% SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+% DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+% OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+% THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+%
 %%%-------------------------------------------------------------------
 %%% @author dark_k3y
-%%% @copyright (C) 2014, <COMPANY>
 %%% @doc
-%%%
+%%% Header file
 %%% @end
 %%%-------------------------------------------------------------------
+
 -author("dark_k3y").
 
 -include("dependencies.hrl").
 
--define(VERSION, "0.1").
+-define(VERSION, "0.1.5").
 
 -define(INITIAL_IP,24).              %% initial max 1/n for basic patterns)
 -define(REMUTATE_PROBABILITY, {4, 5}).
@@ -28,6 +49,7 @@
 -define(PREAMBLE_MAX_BYTES, 32).
 -define(START_MONITOR_ATTEMPTS, 5).
 -define(PROCKET_DIR, "/deps/procket/ebin").
+-define(SERIAL_DIR, "/deps/erlserial/ebin").
 
 %% log levels:
 %% crit, error, warning, issue, info, decision, meta, debug
@@ -44,6 +66,12 @@
 -define(LOAD_PROCKET(R), code:add_pathz(R ++ ?PROCKET_DIR)).
 -else.
 -define(LOAD_PROCKET(R), true).
+-endif.
+
+-ifdef(USE_SERIAL).
+-define(LOAD_SERIAL(R), code:add_pathz(R ++ ?SERIAL_DIR)).
+-else.
+-define(LOAD_SERIAL(R), true).
 -endif.
 
 %% Logging table structure
--- src/r2pipe.erl
+++ src/r2pipe.erl
@@ -1,4 +1,4 @@
-% Copyright (c) 2015-2018 Alexander Bolshev aka dark_k3y
+% Copyright (c) 2015-2019 Alexander Bolshev aka dark_k3y
 %
 % Permission is hereby granted, free of charge, to any person obtaining a copy
 % of this software and associated documentation files (the "Software"), to deal
