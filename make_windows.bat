22053ec5d13fbe6c499737a3590ce5ef0e06c8fe
Darkkey <alexanderdarkkey@gmail.com>
2019-02-10 13:33:20 +0100

    Git commit in version info (2)

--- make_windows.bat
+++ make_windows.bat
@@ -3,6 +3,7 @@ rem disabling procket
 copy rebar.config rebar.config.bak
 copy rebar.config.win rebar.config
 echo %% > src/dependencies.hrl
+echo %% > src/version.hrl
 
 rem now compiling
 escript rebar get-deps compile eunit escriptize skip_deps=true
