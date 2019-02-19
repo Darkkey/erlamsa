47b83eb99155f98f014a6477485a6877168aa45b
Darkkey <alexanderdarkkey@gmail.com>
2019-02-19 20:39:34 +0300

    Added help for simple process monitor script.

--- clients/simple_process_monitor.sh
+++ clients/simple_process_monitor.sh
@@ -4,6 +4,14 @@ PROCESS_NAME=$1
 ERLAMSA_IP=$2
 ERLAMSA_PORT=$3
 
+if [ "$#" -ne 3 ]; then
+	echo "Simple process monitor script for erlamsa"
+	echo ""
+    echo "Illegal number of parameters. Usage $0 <PROCESS_NAME> <ERLAMSA_IP> <ERLAMSA_PORT>"
+	echo ""
+	exit
+fi
+
 while :
 do
 	IS_RUNNING=`ps -aef | grep -i "${PROCESS_NAME}" | grep -v 'grep' | grep -v 'simple_process_monitor' | awk '{ print $3 }'`
